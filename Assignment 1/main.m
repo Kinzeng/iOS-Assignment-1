f4e45671c3ee80a3a2e8aabc47d02569163adb30
Kevin <Zhang@172-16-111-33.dynapool.nyu.edu>
2015-09-26 15:06:41 -0400

    Initial Commit

---
+++ Assignment 1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment 1
+//
+//  Created by Kevin on 9/26/15.
+//  Copyright © 2015 Kevin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
